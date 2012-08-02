3e0f35e62c4e4390b64d1bcdc093590bdf8faebb
aschelch <aurelien.schelcher@gmail.com>
2012-08-02 22:09:24 +0200

    Add dislike feature in the controller

--- Config/routes.php
+++ Config/routes.php
@@ -1,3 +1,4 @@
 <?php
 
 Router::connect('/like/*', array('plugin' => 'like', 'controller' => 'likes', 'action' => 'like'));
+Router::connect('/dislike/*', array('plugin' => 'like', 'controller' => 'likes', 'action' => 'dislike'));
--- Controller/LikesController.php
+++ Controller/LikesController.php
@@ -2,9 +2,8 @@
 
 class LikesController extends AppController{
 	
-	public function like($model, $foreing_id){
-		
-		$Model = ClassRegistry::init($model);
+	public function like($model_name, $foreing_id){
+		$Model = ClassRegistry::init($model_name);
 		$referer = $this->referer('/');
 		$user_id = $this->Auth->user('id'); 
 		
@@ -24,6 +23,30 @@ class LikesController extends AppController{
 		}
 		
 		$Model->like($foreing_id, $user_id);
+		return $this->redirect($referer);
+	}
+	
+	public function dislike($model_name, $foreing_id){
+		$Model = ClassRegistry::init($model_name);
+		$referer = $this->referer('/');
+		$user_id = $this->Auth->user('id');
+	
+		if(!$user_id){
+			$this->Session->setFlash(__('You are not connected.'));
+			return $this->redirect($referer);
+		}
+	
+		if(!$Model->Behaviors->attached('Likeable')){
+			$this->Session->setFlash(__('You cannot like this element.'));
+			return $this->redirect($referer);
+		}
+	
+		if(!$Model->exists($foreing_id)){
+			$this->Session->setFlash(__('This element does not exist.'));
+			return $this->redirect($referer);
+		}
+	
+		$Model->dislike($foreing_id, $user_id);
 		return $this->redirect($referer);
 	}
 	

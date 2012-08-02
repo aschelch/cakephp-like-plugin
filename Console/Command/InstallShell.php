2f5ceab84a84105014c6afc6f747b4647ca3de6d
aschelch <aurelien.schelcher@gmail.com>
2012-08-02 22:18:02 +0200

    Create shell command

---
+++ Console/Command/InstallShell.php
@@ -0,0 +1,19 @@
+<?php
+
+class InstallShell extends AppShell{
+	
+	public function main(){
+		$this->hr();
+		$this->out(__d('like', 'Creation of Likes table'));
+		$this->hr();
+		
+		if (!config('database')) {
+			$this->out(__d('cake_console', 'Your database configuration was not found. Take a moment to create one.'), true);
+			return $this->DbConfig->execute();
+		}else{
+			$this->dispatchShell('schema create --plugin Like');
+			return true;
+		}
+	}
+	
+}

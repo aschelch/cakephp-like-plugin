d7a8dd6ae6c5d4d478bc8df346f57d771c543e5a
aschelch <aurelien.schelcher@gmail.com>
2012-08-05 13:48:57 +0200

    Add Like helper

---
+++ View/Helper/LikeHelper.php
@@ -0,0 +1,30 @@
+<?php
+
+class LikeHelper extends AppHelper{
+	
+	public $helpers = array(
+		'Html'
+	);
+	
+	public function like($model, $foreign_id){
+		return $this->Html->link(__d('like', 'Like'), array(
+			'plugin' => 'like',
+			'controller' => 'likes',
+			'action' => 'like',
+			$model,
+			$foreign_id
+		));
+	}
+	
+	
+	public function dislike($model, $foreign_id){
+		return $this->Html->link(__d('like', 'Dislike'), array(
+			'plugin' => 'like',
+			'controller' => 'likes',
+			'action' => 'dislike',
+			$model,
+			$foreign_id
+		));
+	}
+	
+}

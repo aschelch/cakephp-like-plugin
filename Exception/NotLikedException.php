80cb3b52eaf881297b9193e791aec6d0789aea68
aschelch <aurelien.schelcher@gmail.com>
2012-08-16 21:05:29 +0200

    Correct NotLikedException message

--- Exception/NotLikedException.php
+++ Exception/NotLikedException.php
@@ -3,7 +3,7 @@
 class NotLikedException extends CakeException {
 
 	public function __construct(){
-		parent::__construct(__d('like', 'Already liked'));
+		parent::__construct(__d('like', 'Not liked yet'));
 	}
     
 }

03aa3e16df72d523dad322e0a978b0b5712b07d4
aschelch <aurelien.schelcher@gmail.com>
2012-08-18 21:34:31 +0200

    Make the association with User model modifiable

---
+++ Config/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+$default = array(
+	'user'	 => array(
+		'model' => 'User'
+	)
+);
+Configure::write('Plugin.Like', (Configure::read('Plugin.Like') ? Configure::read('Plugin.Like') : array()) + $default);
--- Model/Like.php
+++ Model/Like.php
@@ -4,9 +4,13 @@
  *
  */
 class Like extends AppModel {
-
-	public $belongsTo = array(
-		'User'
-	);
+	
+	public function __construct( $id = false, $table = NULL, $ds = NULL ){
+		$this->belongsTo['User'] = array(
+			'className' => Configure::read('Plugin.Like.user.model'),
+			'foreignKey'=> 'user_id',
+		);
+		parent::__construct($id,$table,$ds);
+	}
 	
 }

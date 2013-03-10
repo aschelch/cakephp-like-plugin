3f7dde961b214deed62291cc00e476b05d067741
Aurélien Schelcher <aurelien.schelcher@gmail.com>
2013-03-10 13:12:27 +0100

    Add display function in helper

--- View/Helper/LikeHelper.php
+++ View/Helper/LikeHelper.php
@@ -3,7 +3,7 @@
 class LikeHelper extends AppHelper{
 	
 	public $helpers = array(
-		'Html'
+		'Html', 'Text'
 	);
 	
 	public function like($model, $foreign_id){
@@ -25,6 +25,40 @@ class LikeHelper extends AppHelper{
 			$model,
 			$foreign_id
 		));
+	}
+
+	public function display($likes, $userKey = 'username'){
+
+	    $n = count($likes);
+
+	    if($n > 0){
+	        if($n == 1){
+	            $like = $likes[0];
+	            $text = $this->Html->link(
+	                $like['User'][$userKey], 
+	                array('controller'=>'users', 'action'=>'view', $like['User']['id'])
+	            );
+	        }else if($n > 1){
+	            $users = array();
+	            foreach ($likes as $like) {
+	                $users[] = $like['User'][$userKey];
+	            }
+
+	            $text = $this->Html->link(
+	                __n('%s people', '%s people', $n, $n), 
+	                '#', 
+	                array('title'=>$this->Text->toList($users, __('and')))
+	            );
+
+	        }
+	        echo __dn(
+	        	'like',
+	            '%s liked this.',
+	            '%s liked this.',
+	            $n,
+	            $text
+	        );
+	    }
 	}
 	
 }
